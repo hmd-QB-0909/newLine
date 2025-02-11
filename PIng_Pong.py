dd58906de8d27925d2e15a1dbf35edc4a290b38a
hmd-QB-0909 <lhihhmd439@gmail.com>
2025-02-11 12:20:40 +0100

    إصلاح خطأ في دالة calculateSum

--- PIng_Pong.py
+++ PIng_Pong.py
@@ -1,3 +1,9 @@
-print("hello hmd my name is younes ilove my self ")
-print("i can do any thin o every think")
-print("just beleve in your self ")
+name = input("plaise enter your name: ")
+age = input("plaise enter your age: ")
+love = input("plaise enter your love: ")
+def info(name, age , love  ):
+
+    
+    return f"hell {name}, your age is {age}, and you love {love}."
+
+print(info(name,age,love))
